da83a9f36396b6c5e2850dc9cdfdeb02d30ad123
Soheil Hassas Yeganeh <soheil@cs.toronto.edu>
2014-08-22 00:05:01 -0400

    Adds a few tests and fixes a bug.

--- src/go/packet/packet_test.go
+++ src/go/packet/packet_test.go
@@ -3,6 +3,7 @@ package packet
 import (
 	"testing"
 
+	"../../../out/Debug/gen/packet/test/including"
 	"../../../out/Debug/gen/packet/test/simple"
 )
 
@@ -20,5 +21,37 @@ func TestSimplePacket(t *testing.T) {
 
 	if pkt.Size() != 1 {
 		t.Errorf("Size of this simple must be 1 instead of %d.", pkt.Size())
+	}
+}
+
+func TestPolymorphism(t *testing.T) {
+	buf := []byte{2, 4, 1, 1, 1, 1}
+	par := simple.NewSimpleParent(buf, false)
+	if !including.IsIncluding(par) {
+		t.Error("Simple parent should be converatble to including.")
+	}
+
+	inc, err := including.ConvertToIncluding(par)
+	if err != nil {
+		t.Error(err)
+	}
+
+	simples := inc.S()
+	if len(simples) != 2 {
+		t.Errorf("There is %d simples instead of 2.", len(simples))
+	}
+
+	for _, s := range simples {
+		if s.Size() != 1 {
+			t.Errorf("Size of a simple is %d instead of 1.", s.Size())
+		}
+	}
+}
+
+func TestReadArray(t *testing.T) {
+	buf := []byte{2, 4, 1, 1, 1, 1}
+	inc := including.NewAnotherIncluding(buf, false)
+	if inc.Arr() != [2]uint8{1, 1} {
+		t.Errorf("Array in the packet is not correctly loaded: %v", inc.Arr())
 	}
 }
